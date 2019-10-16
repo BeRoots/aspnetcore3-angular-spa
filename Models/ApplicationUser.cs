2f0ea78c3271ff1fd784c109eab57a00ffba10c4
Sebastien Deschamps <wwwbrtswww@gmail.com>
2019-10-16 14:02:03 +0100

    Customize ApplicationUser

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,5 +8,20 @@ namespace Odp.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public static readonly string DEFAULTPREFEREDSTYLE = "default";
+        public string PreferedStyle { get; set; }
+        public string Gender { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Society { get; set; }
+        public string Fulladdress { get; set; }
+        public string Street { get; set; }
+        public UInt32 Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        //public string Email inherit from IdentityUser
+        //public string PhoneNumber inherit from IdentityUser
+        //public string Password inherit from IdentityUser
+        public UInt32 ConnectionTimestamp { get; set; }
     }
 }
